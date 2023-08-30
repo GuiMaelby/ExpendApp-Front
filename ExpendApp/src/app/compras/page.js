5e2263de391de4864f7292b12e428373414b22d6
Guilherme Maelby <107452235+GuiMaelby@users.noreply.github.com>
2023-08-30 14:12:52 -0300

    Add files via upload

---
+++ ExpendApp/src/app/compras/page.js
@@ -0,0 +1,25 @@
+import NavBar from "@/components/NavBar";
+import DataRow from "./DataRow";
+
+async function getContas(){
+  const url = "http://localhost:8080/api/contas"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  return resp.json()
+}
+
+export default async function Contas() {
+  const data = await getContas()
+
+  return (
+    <>
+      <NavBar active={"contas"} />
+
+      <main className="bg-emerald-950 m-12 p-4 rounded">
+        <h2 className="text-2xl font-bold">Compras</h2>
+        <div id="data">
+          {data.map(conta => <DataRow conta={conta} />)}
+        </div>
+      </main>
+    </>
+  )
+}
