0f1ed4de04ad923f7994ba1be1213d11db3a540c
Guilherme Maelby <107452235+GuiMaelby@users.noreply.github.com>
2023-09-14 18:44:34 -0300

    Add files via upload

--- ExpendApp/src/app/compras/DataRow.jsx
+++ ExpendApp/src/app/compras/DataRow.jsx
@@ -1,12 +1,12 @@
 import DropMenu from "@/components/DropMenu";
 import { CreditCardIcon } from "@heroicons/react/24/outline";
 
-export default function DataRow({compra}){
+export default function DataRow({conta}){
     return (
         <div id="data-row" className="flex items-center justify-between hover:bg-slate-800 p-2 rounded cursor-pointer">
             <div className="flex gap-2">
               <CreditCardIcon className="h-6 w-6" />
-              <span>{compra.nome}</span>
+              <span>{conta.nome}</span>
             </div>
             <div className="flex gap-2 items-center">
               <span>R$150.00</span>
--- ExpendApp/src/app/compras/page.js
+++ ExpendApp/src/app/compras/page.js
@@ -1,9 +1,12 @@
 import NavBar from "@/components/NavBar";
 import DataRow from "./DataRow";
+import Button from "@/components/Button";
+import {  PlusIcon } from "@heroicons/react/24/outline";
 
-async function getContas(){
+
+async function getContas() {
   const url = "http://localhost:8080/api/contas"
-  const resp = await fetch(url, { next: { revalidate: 0 } })
+  const resp = await fetch(url)
   return resp.json()
 }
 
@@ -15,7 +18,11 @@ export default async function Contas() {
       <NavBar active={"contas"} />
 
       <main className="bg-emerald-950 m-12 p-4 rounded">
-        <h2 className="text-2xl font-bold">Compras</h2>
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold">Compras</h2>
+          <Button icon={<PlusIcon className="h6 w-6"/>}
+          href="/contas/form">adicionar compras</Button>
+        </div>
         <div id="data">
           {data.map(conta => <DataRow conta={conta} />)}
         </div>
