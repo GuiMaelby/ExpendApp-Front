572d22f3ea81b357b2849c816c57be3647990987
Guilherme Maelby <107452235+GuiMaelby@users.noreply.github.com>
2023-09-29 19:41:34 -0300

    Add files via upload

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Button({children, icon, variant="primary", type="link", ...props}){
     const styles = {
-        primary : "bg-pink-600 hover:bg-pink-800",
+        primary : "bg-lime-600 hover:bg-lime-800",
         secundary: "border-2 border-slate-400 hover:bg-slate-800" 
     }
 
--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,8 +1,8 @@
 export default function InputText({label, id, name, register=()=>{}, ...props}){
     return (
         <div className="flex flex-col gap-1 my-2">
-            <label htmlFor={id} className="text-slate-400">{label}</label>
-            <input id={id} type="text" {...props} className="bg-emerald-200 px-1 py-2 rounded outline-none focus:ring-1 focus:ring-pink-60"/>
+            <label htmlFor={id} className="text-slate-950">{label}</label>
+            <input id={id} type="text" {...props} className="bg-emerald-200 px-1 py-2 rounded outline-none focus:ring-1 focus:ring-lime-500"/>
         </div>
    
     )
