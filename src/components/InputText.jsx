4d9a6f411d0cdb4786fe6ed6d3b73e358c07157d
Guilherme Maelby <107452235+GuiMaelby@users.noreply.github.com>
2023-10-04 16:30:45 -0300

    Add files via upload

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -2,7 +2,7 @@ export default function InputText({label, id, name, register=()=>{}, ...props}){
     return (
         <div className="flex flex-col gap-1 my-2">
             <label htmlFor={id} className="text-slate-500">{label}</label>
-            <input id={id} type="text" {...props} className="bg-emerald-950 px-1 py-2 rounded outline-none focus:ring-1 focus:ring-lime-500"/>
+            <input id={id} type="text" {...props} className="bg-emerald-950 px-1 py-2 rounded outline-none focus:ring-1 focus:ring-green-500"/>
         </div>
    
     )
